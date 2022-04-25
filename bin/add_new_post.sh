685253e3e5bcb6ef9849f1158c6cebfd9bc4adfd
tatsuya tomida <tmd@tatsuyanoMacBook-Pro.local>
2022-04-25 19:02:32 +0900

    shellscript bugfix

--- bin/add_new_post.sh
+++ bin/add_new_post.sh
@@ -4,9 +4,9 @@
 pwd_str=$( pwd )
 soutai=$0
 zettai=$pwd_str${soutai:1}
-cd ${zettai:0:(-20)}
+cd ${zettai:0:${#zettai}-20}
 
 year=$(date "+%Y")
 month=$(date "+%m")
 day=$(date "+%d")
-hugo new posts/$year/$month/$day/index.md
+hugo new posts/$year/$month/$day/$1.md
--- content/posts/2022/04/25/index.md
+++
@@ -1,8 +0,0 @@
----
-title: "25"
-date: 2022-04-25T13:52:11+09:00
-draft: false
-categories:
-tags: 
----
-
